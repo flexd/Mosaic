7ec7ab9b3a5e307d6c4e33fdd95901f8fe9b86c7
Kristoffer Berdal <web@flexd.net>
2012-04-15 00:14:46 +0200

    WIP

--- src/cognitive/ThreeDee.java
+++ src/cognitive/ThreeDee.java
@@ -9,10 +9,13 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
 
 import java.awt.Font;
 import java.io.IOException;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
@@ -20,6 +23,7 @@ import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.UnicodeFont;
@@ -29,6 +33,9 @@ import entities.Quad3D;
 
 
 public class ThreeDee {
+  private static final float zFar = 1000f;
+  private static final float zNear = 0.002f;
+  private static final float fov = 70f;
   public static final int DISPLAY_HEIGHT = 600;
   public static final int DISPLAY_WIDTH = 800;
   
@@ -54,6 +61,8 @@ public class ThreeDee {
   private static org.newdawn.slick.Font bigFont = new UnicodeFont(new Font("Georgia", 1, 20));
   private static org.newdawn.slick.Font mediumFont = new UnicodeFont(new Font("Georgia", 1, 16));
   private static org.newdawn.slick.Font smallFont = new UnicodeFont(new Font("Georgia", 1, 10));
+  
+  private static Matrix4f projectionMatrix;
   
   public void updateFPS() {
     if (Utilities.getTime() - lastFPS > 1000) {
@@ -129,13 +138,18 @@ public class ThreeDee {
      * og +Z er mot kamera.
      */
     //gluPerspective(70f, Display.getWidth() / (float)Display.getHeight(), 0.001f, 1000f);
+    projectionMatrix = LoadPerspective(fov, Display.getWidth() / (float)Display.getHeight(), zNear, zFar);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     
+    
     camera = new Camera3D (new Vector3f(0,0,-10));
+    camera.initMatrix(projectionMatrix);
     renderer = new Renderer3D();
+    renderer.initMatrix(projectionMatrix);
+    
     lastFrame = Utilities.getTime();
     lastFPS = Utilities.getTime();
     //resizeGL();
@@ -178,22 +192,55 @@ public class ThreeDee {
         break;
     }   
   }
+ public FloatBuffer getProjectionMatrix() {
+   FloatBuffer out = BufferUtils.createFloatBuffer(16*4);
+   projectionMatrix.store(out);
+   return (FloatBuffer) out.flip();
+ }
+ public Matrix4f LoadPerspective(float fov, float aspect, float zNear, float zFar)
+ {
+     float f = (float) (1.0f / Math.tan(Math.toRadians(fov)/2.0f));
+     Matrix4f out = new Matrix4f();
+     
+     out.m00 = f / aspect;
+     out.m01 = 0;
+     out.m02 = 0;
+     out.m03 = 0;
+     
+     out.m10 = 0;
+     out.m11 = f;
+     out.m12 = 0;
+     out.m13 = 0;
+     
+     out.m20 = 0;
+     out.m21 = 0;
+     out.m22 = zFar/(zNear-zFar);
+     out.m23 = -1.0f;
+     
+     out.m30 = 0;
+     out.m31 = 0;
+     out.m32 = zNear*zFar/(zNear-zFar);
+     out.m33 = 0;
+     return out;
+ }
   public void run() {
     while(!Display.isCloseRequested()) { //  && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)
+      
       if(Display.isVisible()) {
         updateFPS();
-        camera.controls();
+        
+      
         // Clear for rendering
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
         render();
-        renderer.flushQueue();
+        renderer.flushQueue(camera.getCameraMatrix());
         camera.update(delta); // ALWAYS LAST
         System.out.println("FPS: " + fps);
       }
       else {
         if(Display.isDirty()) {
           render();
-          renderer.flushQueue();
+          renderer.flushQueue(camera.getCameraMatrix());
           camera.update(delta); // ALWAYS LAST
         }
         try {
@@ -202,6 +249,7 @@ public class ThreeDee {
         catch(InterruptedException ex) {
         }
       }
+      projectionMatrix = LoadPerspective(fov, Display.getWidth() / (float)Display.getHeight(), zNear, zFar);
       Display.update();
       Display.sync(60);
     }
